4c44d2c5ddddf8e722d62d3d854a7a847b07b48d
nhat-tan-github <hehhn899@gmail.com>
2026-08-18 10:30:21 +0700

    feat(connect-db): configure dotenv and database connection with docker-compose env

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
+dotenv.config();
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
+connectDB();
 app.use("/api/notes", notesRoutes);
 
 app.listen(5001, () => {
