f9481c930888353174a4daa0b159c3dc11a9b51d
nhat-tan-github <hehhn899@gmail.com>
2026-08-22 16:13:54 +0700

    feat(middleware): add Redis-backed rate limiting

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,19 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
 app.use(express.json());
+app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-  console.log(`server startd on PORT: ${PORT}`);
-  console.log(`Open your browser at: http://localhost:${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server startd on PORT: ${PORT}`);
+    console.log(`Open your browser at: http://localhost:${PORT}`);
+  });
 });
